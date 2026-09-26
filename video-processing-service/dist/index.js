ef0bc64c88a5a6100697302ab1b237c1fd330b24
Astijus Karvauskas <astijusk7@gmail.com>
2026-09-26 04:59:19 +0100

    Post request working. ffmpeg test successfully scales sample input mp4 file down to 360p

--- video-processing-service/dist/index.js
+++ video-processing-service/dist/index.js
@@ -4,7 +4,27 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const fluent_ffmpeg_1 = __importDefault(require("fluent-ffmpeg"));
 const app = (0, express_1.default)();
+app.use(express_1.default.json());
+app.post('/process-video', (req, res) => {
+    const fileIn = req.body.fileIn;
+    const fileOut = req.body.fileOut;
+    if (!fileIn || !fileOut) {
+        return res.status(400).send('Missing fileIn or fileOut parameter');
+    }
+    (0, fluent_ffmpeg_1.default)(fileIn)
+        .outputOptions('-vf', 'scale=-1:360')
+        .on('end', () => {
+        console.log(`Video processing completed. Output file: ${fileOut}`);
+        res.status(200).send(`Video processing completed. Output file: ${fileOut}`);
+    })
+        .on('error', (err) => {
+        console.error("ERROR: ", err.message);
+        res.status(500).send('Error processing video');
+    })
+        .save(fileOut);
+});
 const port = 3000;
 app.get('/', (req, res) => {
     res.send('Video Processing Service is running!');
--- video-processing-service/src/index.ts
+++ video-processing-service/src/index.ts
@@ -1,8 +1,32 @@
 import express from 'express';
+import ffmpeg from 'fluent-ffmpeg';
 
 const app = express();
-const port = 3000;
+app.use(express.json());
 
+
+app.post('/process-video', (req, res) => {
+    const fileIn = req.body.fileIn;
+    const fileOut = req.body.fileOut;
+
+    if (!fileIn || !fileOut) {
+        return res.status(400).send('Missing fileIn or fileOut parameter');
+    }
+
+    ffmpeg(fileIn)
+        .outputOptions('-vf', 'scale=-1:360')
+        .on('end', () => {
+            console.log(`Video processing completed. Output file: ${fileOut}`);
+            res.status(200).send(`Video processing completed. Output file: ${fileOut}`);
+        })
+        .on('error', (err) => {
+            console.error("ERROR: ", err.message);
+            res.status(500).send('Error processing video');
+        })
+        .save(fileOut);
+});
+
+const port = 3000;
 app.get('/', (req, res) => {
     res.send('Video Processing Service is running!');
 });
